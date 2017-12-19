9a21a326deb6f35b31beaa4f6739b68ac4a7489b
Sofiane Gargouri <contact@sofianeg.com>
2017-12-19 14:32:47 +0100

    Adding translations check in CI

--- scripts/check-translations.js
+++ scripts/check-translations.js
@@ -1,4 +1,13 @@
 /*eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
 const languages = ['de', 'en', 'fr', 'nl', 'ru']
+const languageLengths = []
 
-languages.forEach(language => console.log(language, Object.keys(require('../app/i18n/locales/' + language + '.json')).length))
+const allEqual = array => !!array.reduce((a, b) => (a === b) ? a : NaN)
+
+languages.forEach(language => {
+  const languageLength = Object.keys(require('../app/i18n/locales/' + language + '.json')).length
+  languageLengths.push(languageLength)
+  console.log(language, languageLength)
+})
+
+process.exit(allEqual(languageLengths) ? 0 : 1)
